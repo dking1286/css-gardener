f242ac72181b66c6ac4b678e4c6d6a3e235b0443
Daniel King <daniel.oliver.king@gmail.com>
2019-12-04 17:20:36 -0800

    Add functions for getting the unique files matching a list of patterns.

--- src/xyz/dking/css_gardener/utils.clj
+++ src/xyz/dking/css_gardener/utils.clj
@@ -1,7 +1,31 @@
-(ns xyz.dking.css-gardener.utils)
+(ns xyz.dking.css-gardener.utils
+  (:require [me.raynes.fs :as fs]))
 
 (defn map-vals
   "Maps over a seq of key-value pairs, applying the mapping function to
   each value."
   [f & args]
   (apply map (fn [[key val]] [key (f val)]) args))
+
+(defn unique-by
+  "Gets a sequence of unique values in a seq, with uniqueness
+  determined by a passed-in function."
+  ([f seq] (unique-by f seq #{}))
+  ([f seq seen]
+   (cond
+     (nil? seq) nil
+     (empty? seq) '()
+     :else
+     (let [next-item (first seq)
+           next-key (f next-item)]
+       (if (seen next-key)
+         (recur f (rest seq) seen)
+         (lazy-seq (cons next-item
+                         (unique-by f (rest seq) (conj seen next-key)))))))))
+
+(defn unique-files
+  "Gets a seq of all the unique files matching a seq of globs."
+  [globs]
+  (->> globs
+       (mapcat fs/glob)
+       (unique-by #(.getName %))))
--- test/xyz/dking/css_gardener/utils_test.clj
+++ test/xyz/dking/css_gardener/utils_test.clj
@@ -1,5 +1,6 @@
 (ns xyz.dking.css-gardener.utils-test
   (:require [clojure.test :refer :all]
+            [me.raynes.fs :as fs]
             [xyz.dking.css-gardener.utils :as utils]))
 
 (deftest map-vals-test
@@ -9,3 +10,42 @@
   (testing "maps over a seq when called with arity 2"
     (is (= {:hello 2 :world 3}
            (into {} (utils/map-vals inc {:hello 1 :world 2}))))))
+
+(deftest unique-by-test
+  (testing "returns nil when nil is passed in."
+    (is (= nil
+           (utils/unique-by identity nil))))
+  (testing "returns an empty sequence if the passed in sequence is empty"
+    (is (= '()
+           (utils/unique-by identity []))))
+  (testing "returns the original sequence unmodified if the items are already unique"
+    (is (= '("hello" "world")
+           (utils/unique-by identity ["hello" "world"]))))
+  (testing "returns a sequence of unique values"
+    (is (= '("hello" "world")
+           (utils/unique-by identity ["hello" "hello" "world"]))))
+  (testing "uses the passed-in function to determine uniqueness"
+    (is (= '({:name "hello"} {:name "world"})
+           (utils/unique-by :name [{:name "hello"}
+                                   {:name "hello"}
+                                   {:name "world"}])))))
+
+(deftest unique-files-test
+  (testing "returns a list of unique files"
+    (is (= [(fs/absolute "test/xyz/dking/css_gardener/test_example/style_vars.cljs")
+            (fs/absolute "test/xyz/dking/css_gardener/test_example/no_style_vars.cljs")]
+           (utils/unique-files ["test/xyz/dking/css_gardener/test_example/style_vars.cljs"
+                                "test/xyz/dking/css_gardener/test_example/style_vars.cljs"
+                                "test/xyz/dking/css_gardener/test_example/no_style_vars.cljs"]))))
+  (testing "excludes non-existent files"
+    (is (= [(fs/absolute "test/xyz/dking/css_gardener/test_example/style_vars.cljs")
+            (fs/absolute "test/xyz/dking/css_gardener/test_example/no_style_vars.cljs")]
+           (utils/unique-files ["test/xyz/dking/css_gardener/test_example/style_vars.cljs"
+                                "test/xyz/dking/css_gardener/test_example/blah.cljs"
+                                "test/xyz/dking/css_gardener/test_example/no_style_vars.cljs"]))))
+  (testing "expands globs"
+    (is (= [(fs/absolute "test/xyz/dking/css_gardener/test_example/style_vars.cljs")
+            (fs/absolute "test/xyz/dking/css_gardener/test_example/multiple_style_vars.cljs")
+            (fs/absolute "test/xyz/dking/css_gardener/test_example/no_style_vars.cljs")]
+           (utils/unique-files ["test/xyz/dking/css_gardener/test_example/style_vars.cljs"
+                                "test/xyz/dking/css_gardener/test_example/*.cljs"])))))
