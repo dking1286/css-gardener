52a2baaa78c0ed626d026c03e53a73e5d2badf03
Daniel King <daniel.oliver.king@gmail.com>
2019-12-09 21:15:02 -0800

    Add functions for getting the config map from a file or cli args

--- src/xyz/dking/css_gardener/config.clj
+++ src/xyz/dking/css_gardener/config.clj
@@ -1,12 +1,58 @@
 (ns xyz.dking.css-gardener.config
-  (:require [clojure.spec.alpha :as s]))
+  (:require [clojure.edn :as edn]
+            [clojure.spec.alpha :as s]
+            [clojure.string :as string]
+            [clojure.tools.cli :as cli])
+  (:import [java.io FileNotFoundException]))
 
 (s/def ::type keyword?)
 (s/def ::input-files (s/coll-of string? :kind vector? :min-count 1))
 (s/def ::output-file string?)
 
-(s/def ::config (s/keys :req-un [::type
-                                 ::input-files
-                                 ::output-file]))
+(s/def ::config (s/keys :req-un [::type ::input-files ::output-file]))
 
+(defn from-file
+  "Gets a configuration map from a configuration file.
 
+  When filename is not supplied, defaults to css-gardener.edn"
+  ([] (from-file "css-gardener.edn"))
+  ([filename]
+   (try
+     {:status :success
+      :result (edn/read-string (slurp filename))}
+     (catch FileNotFoundException e
+       {:status :failure
+        :reason :config-file-not-found
+        :error e})
+     (catch RuntimeException e
+       {:status :failure
+        :reason :config-file-invalid
+        :error e}))))
+
+(defn- parse-input-files
+  [input-files]
+  (->> (string/split input-files #",")
+       (filter (complement empty?))
+       (into [])))
+
+(def ^:private cli-options
+  [["-t" "--type TYPE"
+    "Type of stylesheet"
+    :parse-fn keyword]
+
+   ["-i" "--input-files LIST OF INPUT FILES"
+    "Comma-separated list of input files"
+    :default []
+    :parse-fn parse-input-files]
+
+   ["-o" "--output-file OUTPUT FILE"
+    "Output file name"]
+
+   ["-w" "--watch"
+    "Whether or not to watch and rebuild on change"]
+
+   ["-h" "--help"]])
+
+(defn from-cli-args
+  [args]
+  (:options (cli/parse-opts args cli-options)))
---
+++ test/xyz/dking/css_gardener/config_test.clj
@@ -0,0 +1,42 @@
+(ns xyz.dking.css-gardener.config-test
+  (:require [clojure.edn :as edn]
+            [clojure.test :refer :all]
+            [xyz.dking.css-gardener.config :as config]
+            [xyz.dking.css-gardener.test-helpers :refer [*temp-file*
+                                                         with-temp-file]]))
+
+(use-fixtures :each with-temp-file)
+
+(deftest from-file-test
+  (testing "config file not found"
+    (let [result (config/from-file "i-do-not-exist")]
+      (is (= :failure (:status result)))
+      (is (= :config-file-not-found (:reason result)))))
+  (testing "config file does not contain edn"
+    (spit *temp-file* "{\"hello\": \"world\"}")
+    (let [result (config/from-file *temp-file*)]
+      (is (= :failure (:status result)))
+      (is (= :config-file-invalid (:reason result)))))
+  (testing "config file valid"
+    (spit *temp-file* "{:type :garden}")
+    (let [result (config/from-file *temp-file*)]
+      (is (= :success (:status result)))
+      (is (= {:type :garden} (:result result))))))
+
+(deftest from-cli-args-test
+  (testing "parsing cli args"
+    (let [result (config/from-cli-args ["-h"])]
+      (is (true? (:help result)))))
+  (testing "parsing type"
+    (let [result (config/from-cli-args ["-t" "garden"])]
+      (is (= :garden (:type result)))))
+  (testing "parsing input-files"
+    (let [result (config/from-cli-args ["-i" "hello.clj,world.clj"])]
+      (is (= ["hello.clj" "world.clj"]
+             (:input-files result)))))
+  (testing "missing input-files"
+    (let [result (config/from-cli-args ["-i"])]
+      (is (= [] (:input-files result)))))
+  (testing "empty input-files"
+    (let [result (config/from-cli-args ["-i" ""])]
+      (is (= [] (:input-files result))))))
