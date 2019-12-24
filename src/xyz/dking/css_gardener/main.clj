ff19dea003f4b7d57b882313c948e593be4f52ab
Daniel King <daniel.oliver.king@gmail.com>
2019-12-23 21:42:41 -0800

    Support "init" command in main function.

--- src/xyz/dking/css_gardener/main.clj
+++ src/xyz/dking/css_gardener/main.clj
@@ -1,7 +1,6 @@
 (ns xyz.dking.css-gardener.main
-  (:require [xyz.dking.css-gardener.builder :as builder]
-            [xyz.dking.css-gardener.config :as config]
-            [xyz.dking.css-gardener.core :refer [build watch]]))
+  (:require [xyz.dking.css-gardener.config :as config]
+            [xyz.dking.css-gardener.core :refer [build init watch]]))
 
 (def help-message
   "TODO")
@@ -21,24 +20,20 @@
 (defn main
   "The main process of css-gardener."
   [command & args]
-  (let [{:keys [help watch config-file] :as cli-config}
-        (config/from-cli-args args)
-
-        {status :status reason :reason file-config :result}
-        (config/from-file config-file)
-
-        config
-        (merge file-config cli-config)]
-    (if (or (= command "--help") (= command "-h"))
-      (println help-message)
+  (let [{:keys [config-file] :as cli-config} (config/from-cli-args args)
+        {:keys [status reason result]} (config/from-file config-file)
+        config (merge result cli-config)]
+    (cond
+      (or (= command "--help") (= command "-h")) (println help-message)
+      (= command "init") (init config)
+      :else
       (do
         (when (missing-default-config-file? status config-file)
           (println (str "Default configuration file " config/default-config-file
                         " not found, using command line args.")))
         (when (missing-custom-config-file? status config-file)
           (println (str "WARNING: Configuration file " config-file
-                        " not found, using only command line args.")))
-        
+                        " not found, using only command line args.")))      
         (case command
           "watch" (watch config)
           "build" (build config)
