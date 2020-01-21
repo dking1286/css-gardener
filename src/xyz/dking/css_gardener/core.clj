a2dbde2a96141e16759f5d9f029a4e226baf95ce
Daniel King <daniel.oliver.king@gmail.com>
2020-01-20 18:35:38 -0800

    Update core namespace to support sass builder and project initialization

--- src/xyz/dking/css_gardener/core.clj
+++ src/xyz/dking/css_gardener/core.clj
@@ -1,12 +1,15 @@
 (ns xyz.dking.css-gardener.core
   (:require [xyz.dking.css-gardener.builder :as builder]
-            [xyz.dking.css-gardener.builder.garden :as garden]))
+            [xyz.dking.css-gardener.builder.garden :as garden]
+            [xyz.dking.css-gardener.builder.sass :as sass]
+            [xyz.dking.css-gardener.init :as init]))
 
 (defn- get-builder
   "Gets an instance of the builder type specified in the config map."
   [config]
   (case (:type config)
     :garden (garden/new-builder config)
+    :sass (sass/new-builder config)
     (throw (ex-info
             (str "Unknown :type property " (:type config) " found in config.")
             {:type :unknown-builder-type}))))
@@ -14,7 +17,7 @@
 (defn init
   "Initializes a css-gardener project in the current directory."
   [config]
-  (throw (UnsupportedOperationException. "Init not implemented yet.")))
+  (init/initialize-project config))
 
 (defn build
   "Executes a single build of the user's stylesheet."
