2260a21661d9fbc8dba23e74839b948a49ffe6a1
Daniel King <daniel.oliver.king@gmail.com>
2020-02-29 12:59:29 -0800

    Add function to read files based on a glob pattern

--- src/xyz/dking/css_gardener/utils/fs.cljs
+++ src/xyz/dking/css_gardener/utils/fs.cljs
@@ -1,11 +1,17 @@
 (ns xyz.dking.css-gardener.utils.fs
   (:refer-clojure :exclude [exists?])
   (:require ["fs" :as fs]
+            ["glob" :as node-glob]
             [clojure.core.async :refer [go >! chan]]
             [clojure.string :as string]
             [xyz.dking.css-gardener.utils.async :refer [callback->channel
                                                         node-callback->channel]]
             [xyz.dking.css-gardener.utils.errors :as errors]))
+
+(defn glob
+  [pattern]
+  (node-callback->channel
+    node-glob pattern (fn [err files] (or err (vec files)))))
 
 (defn exists?
   "Determine if a file exists, and that the process has read permission."
@@ -16,5 +22,5 @@
 (defn read-file
   "Reads the contents of a file as a UTF-8 string"
   [filename]
-  (node-callback->channel fs/readFile filename "utf8"
-                          (fn [err contents] (or err contents))))
+  (node-callback->channel
+    fs/readFile filename "utf8" (fn [err contents] (or err contents))))
--- test/xyz/dking/css_gardener/utils/fs_test.cljs
+++ test/xyz/dking/css_gardener/utils/fs_test.cljs
@@ -5,6 +5,30 @@
             [xyz.dking.css-gardener.utils.errors :as errors]
             [clojure.string :as string]))
 
+(deftest glob-not-found
+  (testing "returns a channel that yields an empty seq when no files match the glob"
+    (async done
+      (go
+        (is (= [] (<! (sut/glob "/blah/*.cljs"))))
+        (done)))))
+
+(deftest glob-one-star
+  (testing "returns a channel that yields a set of file paths matching the pattern when there is one star in the pattern"
+    (async done
+      (go
+        (is (= ["test/xyz/dking/css_gardener/test_example/nested/no_style_vars.cljs"]
+               (<! (sut/glob "test/xyz/dking/css_gardener/test_example/nested/*.cljs"))))
+        (done)))))
+
+(deftest glob-double-star
+  (testing "returns a channel that yields a set of file paths matching the pattern when there is a recursive double star in the pattern"
+    (async done
+      (go
+        (is (= ["test/xyz/dking/css_gardener/test_example/nested/nested/no_style_vars.cljs"
+                "test/xyz/dking/css_gardener/test_example/nested/no_style_vars.cljs"]
+               (<! (sut/glob "test/xyz/dking/css_gardener/test_example/nested/**/*.cljs"))))
+        (done)))))
+
 (deftest exists-no-file-test
   (testing "returns a channel that yields false when the file does not exist"
     (async done
@@ -30,7 +54,7 @@
   (testing "returns a channel that yields an error when an unknown error happens while reading the file"))
 
 (deftest read-file-exists
-  (testing "returs a channel that yields the file contents as a string when reading is successful"
+  (testing "returns a channel that yields the file contents as a string when reading is successful"
     (async done
       (go
         (is (string/includes? (<! (sut/read-file "./package.json"))
