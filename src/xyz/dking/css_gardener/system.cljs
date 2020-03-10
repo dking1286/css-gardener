9737de1e6a78e6d29105b75b3e663739b9f87194
Daniel King <daniel.oliver.king@gmail.com>
2020-03-10 06:38:27 -0700

    Import fs into system to ensure it is compiled

--- src/xyz/dking/css_gardener/system.cljs
+++ src/xyz/dking/css_gardener/system.cljs
@@ -1,6 +1,7 @@
 (ns xyz.dking.css-gardener.system
   (:require [com.stuartsierra.component :as component]
-            [xyz.dking.css-gardener.logging :as logging]))
+            [xyz.dking.css-gardener.logging :as logging]
+            [xyz.dking.css-gardener.utils.fs]))
 
 (defn system
   [{:keys [log-level]
