51da4a47801af03d3cb30608524446fce42e92b2
Daniel King <daniel.oliver.king@gmail.com>
2020-04-24 21:26:05 -0700

    Add code to parse namespace dependencies from an ns form

---
+++ packages/css-gardener-core/src/css_gardener/core/cljs_parsing.cljs
@@ -0,0 +1,74 @@
+(ns css-gardener.core.cljs-parsing)
+
+;; This is a copy of part of the clojure.tools.namespace.parse
+;; namespace. Instead of copying this, fork the repo and publish my own
+;; fork on Clojars.
+;; 
+;; https://github.com/clojure/tools.namespace
+;; https://github.com/clojure/tools.namespace/blob/master/src/main/clojure/clojure/tools/namespace/parse.cljc#L61
+
+;;; Parsing dependencies
+
+(defn- prefix-spec?
+  "Returns true if form represents a libspec prefix list like
+  (prefix name1 name1) or [com.example.prefix [name1 :as name1]]"
+  [form]
+  (and (sequential? form)  ; should be a list, but often is not
+       (symbol? (first form))
+       (not-any? keyword? form)
+       (< 1 (count form))))  ; not a bare vector like [foo]
+
+(defn- option-spec?
+  "Returns true if form represents a libspec vector containing optional
+  keyword arguments like [namespace :as alias] or
+  [namespace :refer (x y)] or just [namespace]"
+  [form]
+  (and (sequential? form)  ; should be a vector, but often is not
+       (or (symbol? (first form)) ;; Local change (handling string namespaces)
+           (string? (first form)))
+       (or (keyword? (second form))  ; vector like [foo :as f]
+           (= 1 (count form)))))  ; bare vector like [foo]
+
+(defn- deps-from-libspec [prefix form]
+  (cond (prefix-spec? form)
+        (mapcat (fn [f] (deps-from-libspec
+                         (symbol (str (when prefix (str prefix "."))
+                                      (first form)))
+                         f))
+                (rest form))
+        (option-spec? form)
+        (deps-from-libspec prefix (first form))
+        (symbol? form)
+        (list (symbol (str (when prefix (str prefix ".")) form)))
+        (keyword? form)  ; Some people write (:require ... :reload-all)
+        nil
+        (string? form) ;; Local change (handling string namespaces)
+        (list form)
+        :else
+        (throw (ex-info "Unparsable namespace form"
+                        {:reason ::unparsable-ns-form
+                         :form form}))))
+
+(def ^:private ns-clause-head-names
+  "Set of symbol/keyword names which can appear as the head of a
+  clause in the ns form."
+  #{"use" "require"})
+
+(def ^:private ns-clause-heads
+  "Set of all symbols and keywords which can appear at the head of a
+  dependency clause in the ns form."
+  (set (mapcat (fn [name] (list (keyword name)
+                                (symbol name)))
+               ns-clause-head-names)))
+
+(defn- deps-from-ns-form [form]
+  (when (and (sequential? form)  ; should be list but sometimes is not
+             (contains? ns-clause-heads (first form)))
+    (mapcat #(deps-from-libspec nil %) (rest form))))
+
+(defn deps-from-ns-decl
+  "Given an (ns...) declaration form (unevaluated), returns a set of
+  symbols naming the dependencies of that namespace.  Handles :use and
+  :require clauses but not :load."
+  [decl]
+  (set (mapcat deps-from-ns-form decl)))
