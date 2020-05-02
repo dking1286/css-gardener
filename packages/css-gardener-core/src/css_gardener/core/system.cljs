057537ee68ca27817fb7c0ede5dd926cbc5e9266
Daniel King <daniel.oliver.king@gmail.com>
2020-05-02 10:28:11 -0700

    Add logging namespace to wrap goog.log

--- packages/css-gardener-core/src/css_gardener/core/system.cljs
+++ packages/css-gardener-core/src/css_gardener/core/system.cljs
@@ -1,16 +1,21 @@
 (ns css-gardener.core.system
   (:require [css-gardener.core.cljs-parsing :as cljs]
             [css-gardener.core.dependency :as dependency]
+            [css-gardener.core.logging :as logging]
             [css-gardener.core.modules :as modules]
             [css-gardener.core.utils.fs :as fs]
             [integrant.core :as ig]))
 
 (def config
-  {::modules/load {}
+  {::logging/logger {:level :info
+                     :sinks #{:console}}
+
+   ::modules/load {}
 
    ::fs/exists? {}
    ::fs/read-file {}
 
    ::cljs/deps {:exists? (ig/ref ::fs/exists?)}
-   ::dependency/deps {:load-module (ig/ref ::modules/load)
+   ::dependency/deps {:logger (ig/ref ::logging/logger)
+                      :load-module (ig/ref ::modules/load)
                       :cljs-deps (ig/ref ::cljs/deps)}})
