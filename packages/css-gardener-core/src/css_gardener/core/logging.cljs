5d0187dfa702447bcb493a6eb35dd2836ed425f6
Daniel King <daniel.oliver.king@gmail.com>
2020-05-02 10:28:11 -0700

    Add logging namespace to wrap goog.log

---
+++ packages/css-gardener-core/src/css_gardener/core/logging.cljs
@@ -0,0 +1,67 @@
+(ns css-gardener.core.logging
+  (:require [clojure.pprint :refer [pprint]]
+            [clojure.spec.alpha :as s]
+            [goog.log :as log]
+            [integrant.core :as ig])
+  (:import [goog.log Level Logger]))
+
+(s/def ::sink #{:console :cache})
+(s/def ::level #{:debug :info :warning :error})
+(s/def ::goog-logger #(instance? Logger %))
+(s/def ::cache #(satisfies? IDeref %))
+(s/def ::handler fn?)
+(s/def ::handlers (s/coll-of ::handler))
+(s/def ::logger (s/keys :req-un [::goog-logger ::cache ::handlers]))
+
+(def goog-level
+  {:debug Level/FINE
+   :info Level/INFO
+   :warning Level/WARNING
+   :error Level/SEVERE})
+
+(defn log-fn
+  [f]
+  (fn [logger msg]
+    (f (:goog-logger logger) msg)))
+
+(s/fdef debug
+  :args (s/cat :logger ::logger))
+
+(def debug (log-fn log/fine))
+
+(s/fdef info
+  :args (s/cat :logger ::logger))
+
+(def info (log-fn log/info))
+
+(s/fdef warning
+  :args (s/cat :logger ::logger))
+
+(def warning (log-fn log/warning))
+
+(s/fdef error
+  :args (s/cat :logger ::logger))
+
+(def error (log-fn log/error))
+
+(defmethod ig/init-key ::logger
+  [_ {:keys [sinks level]
+      :or {sinks #{} level :info}}]
+  (let [level (goog-level level)
+        goog-logger (log/getLogger "css-gardener.core.logging" level)
+        cache (atom [])
+        console-handler #(pprint (.getMessage %))
+        cache-handler #(swap! cache conj %)]
+    (when (contains? sinks :console)
+      (log/addHandler goog-logger console-handler))
+    (when (contains? sinks :cache)
+      (log/addHandler goog-logger cache-handler))
+    {:goog-logger goog-logger
+     :cache cache
+     :handlers [console-handler cache-handler]}))
+
+(defmethod ig/halt-key! ::logger
+  [_ {:keys [logger cache handlers]}]
+  (reset! cache [])
+  (doseq [handler handlers]
+    (log/removeHandler logger handler)))
