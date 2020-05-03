2ea5485fa8545f8b7a1ef3500597683bd405959e
Daniel King <daniel.oliver.king@gmail.com>
2020-05-03 13:11:22 -0700

    Add lint script, fix a few lint errors

--- packages/css-gardener-core/src/css_gardener/core/utils/testing.cljs
+++ packages/css-gardener-core/src/css_gardener/core/utils/testing.cljs
@@ -1,5 +1,5 @@
 (ns css-gardener.core.utils.testing
-  (:require-macros [css-gardener.core.utils.testing :refer [with-system]])
+  (:require-macros [css-gardener.core.utils.testing])
   (:require [clojure.spec.test.alpha :as stest]))
 
 (def instrument-specs {:before #(stest/instrument)
