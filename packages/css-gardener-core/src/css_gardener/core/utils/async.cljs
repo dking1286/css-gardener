83bbfbdd434f0d43a498c61903f9e4303918b27d
Daniel King <daniel.oliver.king@gmail.com>
2020-05-07 07:37:52 -0700

    Add more tests and documentation for async utils

--- packages/css-gardener-core/src/css_gardener/core/utils/async.cljs
+++ packages/css-gardener-core/src/css_gardener/core/utils/async.cljs
@@ -10,15 +10,26 @@
   (fn [& _] (go val)))
 
 (defn callback->channel
+  "Takes a function that takes a callback, and returns a channel that yields
+   the value returned by the callback. Used to convert callback-based async
+   functions into channel-based."
   [f & args]
   (let [out-chan (chan)]
     (go
       (let [f-args (vec (butlast args))
             transformer (last args)
             callback (fn [& cb-args]
-                       (put! out-chan (apply transformer cb-args))
-                       (close! out-chan))]
-        (apply f (conj f-args callback))))
+                       (let [out-val (try
+                                       (apply transformer cb-args)
+                                       (catch js/Error err
+                                         err))]
+                         (put! out-chan out-val)
+                         (close! out-chan)))]
+        (try
+          (apply f (conj f-args callback))
+          (catch js/Error err
+            (put! out-chan err)
+            (close! out-chan)))))
     out-chan))
 
 (defn- wrap-error-first-fn
@@ -27,18 +38,26 @@
     (apply f (errors/wrap-node-error err) args)))
 
 (defn node-callback->channel
+  "Takes a function that takes a node-style error-first callback, and returns
+   a channel that yields the value returned by the callback. Used to convert
+   a node-style callback-based async function into channel-based."
   [f & args]
   (let [f-args (vec (butlast args))
         transformer (wrap-error-first-fn (last args))]
     (apply callback->channel f (conj f-args transformer))))
 
-(defn transform
+(defn- transform
   [xform ch]
   (let [out (chan 1 xform)]
     (pipe ch out)
     out))
 
 (defn map
+  "Takes a function and a channel, and returns a channel that yields all values
+   from the input channel, transformed with the function.
+   
+   If the input channel yields an error, it is propagated to the returned
+   channel without being transformed."
   [f ch]
   (transform (cljs.core/map (fn [x]
                               (if (errors/error? x)
@@ -50,6 +69,10 @@
              ch))
 
 (defn flat-map
+  "Calls f with each value from ch, and returns a channel that yields all values
+   yielded by the channels returned by f.
+   
+   (a -> Channel b) -> Channel a -> Channel b"
   [f ch]
   (let [out-chan (chan)]
     (go-loop []
@@ -67,16 +90,22 @@
     out-chan))
 
 (defn trace
+  "Returns a channel that prints and then yields each value from the input
+   channel.
+   
+   Useful for debugging chains of async operations."
   ([ch] (trace nil ch))
   ([prefix ch]
    (transform (cljs.core/map (fn [x]
                                (if prefix
-                                 (println (str prefix x))
+                                 (println (str prefix " " x))
                                  (println x))
                                x))
               ch)))
 
 (defn take-all
+  "Repeatedly takes from the input channel until it closes, then yields a
+   single seq of all values received from the input channel."
   [timeout-millis ch & {:keys [bailout-on-error?]}]
   (let [timeout-ch (timeout timeout-millis)]
     (go-loop [results []]
@@ -88,5 +117,8 @@
           :else (recur (conj results result)))))))
 
 (defn await-all
+  "Waits for all of the input channels to close, then yields a seq of all
+   values received from them. If any of the input channels yields an error,
+   immediately yield the error."
   [timeout-millis chs]
   (take-all timeout-millis (merge chs) :bailout-on-error? true))
