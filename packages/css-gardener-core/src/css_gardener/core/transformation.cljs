8159c723d010dbdfdeb1c095dc2d7b966006d607
Daniel King <daniel.oliver.king@gmail.com>
2020-05-24 11:55:13 -0700

    1. Add spec to check if an object is a valid transformer 2. Wrap each loaded transformer in a map, so that each one can have its module name attached

--- packages/css-gardener-core/src/css_gardener/core/transformation.cljs
+++ packages/css-gardener-core/src/css_gardener/core/transformation.cljs
@@ -1,16 +1,29 @@
 (ns css-gardener.core.transformation
   (:require [clojure.core.async :refer [go]]
+            [clojure.spec.alpha :as s]
             [css-gardener.core.logging :as logging]
             [css-gardener.core.modules :as modules]
+            [goog.object :as gobj]
             [integrant.core :as ig]))
+
+(defn transformer?
+  "Determines whether or not a value is a valid transformer."
+  [val]
+  (boolean (and (gobj/get val "enter")
+                (gobj/get val "exit"))))
+
+(s/def ::transformer transformer?)
+(s/def ::options map?)
+(s/def ::transformer-config
+  (s/keys :req-un [::modules/module ::transformer]))
 
 (defn transformer-stub
   "Creates a stub transformer for use in tests."
   [err result]
-  (fn [_ _ callback]
-    ;; Call the callback asynchronously so that tests can't rely on it being
-    ;; synchronous
-    (go (callback err result))))
+  #js {:enter (fn [_ _ callback]
+                (go (callback err result)))
+       :exit (fn [_ _ callback]
+               (go (callback err result)))})
 
 (defmethod ig/init-key ::transformers
   [_ {:keys [config logger load-module]}]
@@ -18,6 +31,9 @@
   (->> (:rules config)
        vals
        (mapcat :transformers)
-       (map modules/extract-module)
-       (map #(vector % (load-module %)))
+       (map (fn [config]
+              (let [module (modules/extract-module config)]
+                {:module module
+                 :transformer (load-module module)})))
+       (map #(vector (:module %) %))
        (into {})))
