59e2a685a15c413d67b6bab4f246437d9c06b4dc
Daniel King <daniel.oliver.king@gmail.com>
2020-05-30 13:09:21 -0700

    Implement sass dependency resolver

--- packages/sass-resolver/src/css_gardener/sass_resolver/core.cljs
+++ packages/sass-resolver/src/css_gardener/sass_resolver/core.cljs
@@ -1,6 +1,40 @@
-(ns css-gardener.sass-resolver.core)
+(ns css-gardener.sass-resolver.core
+  (:require [goog.array :as array]
+            [goog.object :as gobj]
+            [sass]))
 
 (defn main
   "Dependency resolver for scss and sass stylesheets."
-  [_ _ ^js/Function callback]
-  (callback nil #js []))
+  [file options ^js/Function callback]
+  (cond
+    (not (gobj/containsKey file "absolutePath"))
+    (callback (js/Error. "'absolutePath' key is missing on the input file") nil)
+
+    (not (gobj/containsKey file "content"))
+    (callback (js/Error. "'content' key is missing on the input file") nil)
+
+    :else
+    (let [absolute-path (gobj/get file "absolutePath")
+          content (gobj/get file "content")]
+      (sass/render
+       (js/Object.assign #js {:file absolute-path
+                              :data content}
+                         options)
+       (fn [err result]
+         (if err
+           (callback err nil)
+           ;; TODO: This will cause some files to be
+           ;; duplicated in the dependency graph, because
+           ;; dependencies of the file.
+           ;; includedFiles includes all *transitive*
+           ;; Not a big deal, because sass files usually
+           ;; don't have a deep dependency graph.
+           ;; 
+           ;; A better long-term solution: Contribute to
+           ;; sass-graph to make it support the new
+           ;; @use syntax.
+           ;; https://www.npmjs.com/package/sass-graph
+           (callback nil (-> result
+                             (gobj/get "stats")
+                             (gobj/get "includedFiles")
+                             (array/filter #(not= % absolute-path))))))))))
--- packages/sass-resolver/src/css_gardener/sass_resolver/core_test.cljs
+++ packages/sass-resolver/src/css_gardener/sass_resolver/core_test.cljs
@@ -1,15 +1,98 @@
 (ns css-gardener.sass-resolver.core-test
   (:require [clojure.test :refer [deftest testing is async]]
             [css-gardener.sass-resolver.core :refer [main]]
-            [goog.array :as array]))
+            [fs]
+            [goog.array :as array]
+            [path]))
+
+(defn- test-stylesheet-path
+  [name]
+  (path/resolve (str "src/css_gardener/sass_resolver/"
+                     "test_stylesheets/"
+                     name)))
+
+(defn- test-stylesheet
+  [name]
+  (let [abs-path (test-stylesheet-path name)]
+    #js {:absolutePath abs-path
+         :content (fs/readFileSync abs-path "utf8")}))
+
+(deftest t-main-no-absolute-path
+  (testing "Yields an error if the input object is missing the
+            absolutePath key"
+    (async done
+      (main #js {:content ""}
+            #js {}
+            (fn [err result]
+              (is (nil? result))
+              (is (= "'absolutePath' key is missing on the input file"
+                     (.-message err)))
+              (done))))))
+
+(deftest t-main-no-content
+  (testing "Yields an error if the input object is missing the content key"
+    (async done
+      (main #js {:absolutePath "/some/style/file.scss"}
+            #js {}
+            (fn [err result]
+              (is (nil? result))
+              (is (= "'content' key is missing on the input file"
+                     (.-message err)))
+              (done))))))
+
+(deftest t-main-sass-syntax-error
+  (testing "Yields an error if the input file has a syntax error"
+    (async done
+      (main #js {:absolutePath "/some/style/file.scss"
+                 :content "@import \"blah\";
+                           
+                           .hello {"}
+            #js {}
+            (fn [err result]
+              (is (nil? result))
+              (is (instance? js/Error err))
+              (done))))))
 
 (deftest t-main-no-deps
   (testing "Yields an empty array if the sass file has no dependencies"
     (async done
-      (main #js {:absolutePath "/some/style/file.scss"
-                 :content ""}
+      (main (test-stylesheet "no_deps.scss")
             #js {}
             (fn [error result]
               (is (nil? error))
               (is (array/isEmpty result))
+              (done))))))
+
+(deftest t-main-import-dependencies
+  (testing "Detects dependencies with the @import directive"
+    (async done
+      (main (test-stylesheet "import.scss")
+            #js {}
+            (fn [error result]
+              (is (nil? error))
+              (is (array/equals #js [(test-stylesheet-path "dependency.scss")]
+                                result))
+              (done))))))
+
+(deftest t-main-use-dependencies
+  (testing "Detects dependencies with the @use directive"
+    (async done
+      (main (test-stylesheet "use.scss")
+            #js {}
+            (fn [error result]
+              (is (nil? error))
+              (is (array/equals #js [(test-stylesheet-path "dependency.scss")]
+                                result))
+              (done))))))
+
+(deftest t-main-multiple-dependencies
+  (testing "Detects multiple dependencies"
+    (async done
+      (main (test-stylesheet "multi_use.scss")
+            #js {}
+            (fn [error result]
+              (is (nil? error))
+              (is (array/equals #js [(test-stylesheet-path "dependency.scss")
+                                     (test-stylesheet-path "dependency_2.scss")]
+                                (.sort result)))
               (done))))))
