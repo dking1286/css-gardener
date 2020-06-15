1dde05df3adac086a9d9dfd928fde3d1fcb7c0de
Daniel King <daniel.oliver.king@gmail.com>
2020-06-15 07:31:37 -0700

    Add skeleton of scope-transformer subpackage

---
+++ packages/scope-transformer/src/css_gardener/scope_transformer/core_dev.cljs
@@ -0,0 +1,11 @@
+(ns css-gardener.scope-transformer.core-dev)
+
+(defn enter
+  "TODO: Add me"
+  [file _ callback]
+  (callback nil file))
+
+(defn exit
+  "TODO: Add me"
+  [file _ callback]
+  (callback nil file))
