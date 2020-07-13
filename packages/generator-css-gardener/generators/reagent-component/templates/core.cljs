86632d5140eacb2af1e787956f0a97208c2ab41d
Daniel King <daniel.oliver.king@gmail.com>
2020-07-13 08:28:08 -0700

    Generate component files from the templates

--- packages/generator-css-gardener/generators/reagent-component/templates/core.cljs
+++ packages/generator-css-gardener/generators/reagent-component/templates/core.cljs
@@ -1,11 +1,11 @@
-(ns ^{:css-gardener/require ["./param-style-name"]}
-  param-components-path.param-component-name.core
+(ns ^{:css-gardener/require ["./<%= styleName %>"]}
+  <%= nsPrefix %>.<%= componentName %>.core
   (:require [css-gardener.scope.core :refer [scoped-classname infer-scope]]))
 
 (def ^:private scoped (memoize (partial scoped-classname (infer-scope))))
 
-(defn param-component-name
+(defn <%= componentName %>
   "FIXME write a docstring."
   []
   [:div {:className (scoped "container")}
-   "I am component param-component-name"])
+   "I am component <%= componentName %>"])
