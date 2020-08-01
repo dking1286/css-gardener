700a09ae6bd129b9074bd024935304123f9b9c5d
Daniel King <daniel.oliver.king@gmail.com>
2020-07-31 19:52:25 -0700

    Implement postcss transformer

--- packages/postcss-transformer/src/css_gardener/postcss_transformer/core.cljs
+++ packages/postcss-transformer/src/css_gardener/postcss_transformer/core.cljs
@@ -1,9 +1,43 @@
-(ns css-gardener.postcss-transformer.core)
+(ns css-gardener.postcss-transformer.core
+  (:require [css-gardener.common.node-modules :refer [root-module-path]]
+            [goog.object :as gobj]
+            ["postcss" :as postcss]))
+
+(defn- load-plugin
+  [plugin-spec]
+  (let [module (-> (gobj/get plugin-spec "nodeModule")
+                   root-module-path
+                   js/require)]
+    (if (gobj/containsKey plugin-spec "options")
+      (module (gobj/get plugin-spec "options"))
+      module)))
+
+(defn- load-plugins
+  [plugin-specs]
+  (->> plugin-specs
+       (map load-plugin)
+       js/Array.from))
 
 (defn enter
-  [file _ callback]
-  (callback nil file))
+  "Entry point for the postcss transformer."
+  [file options callback]
+  (cond
+    (not (gobj/containsKey file "absolutePath"))
+    (callback (js/Error. "'absolutePath' key is missing on the input file") nil)
+
+    (not (gobj/containsKey file "content"))
+    (callback (js/Error. "'content' key is missing on the input file") nil)
+
+    :else
+    (let [plugins (load-plugins (gobj/get options "plugins"))]
+      (-> (postcss plugins)
+          (.process (gobj/get file "content"))
+          (.then (fn [result]
+                   (let [outfile (gobj/clone file)]
+                     (gobj/set outfile "content" (gobj/get result "css"))
+                     (callback nil outfile))))))))
 
 (defn exit
+  "Exit point for the postcss transformer. Returns the file unmodified."
   [file _ callback]
   (callback nil file))
--- packages/postcss-transformer/src/css_gardener/postcss_transformer/core_test.cljs
+++ packages/postcss-transformer/src/css_gardener/postcss_transformer/core_test.cljs
@@ -1,1 +1,77 @@
-(ns css-gardener.postcss-transformer.core-test)
+(ns css-gardener.postcss-transformer.core-test
+  (:require [clojure.test :refer [deftest async testing is]]
+            [css-gardener.common.js :refer [to-js]]
+            [css-gardener.postcss-transformer.core :refer [enter exit]]
+            [fs]
+            [goog.object :as gobj]
+            [path]))
+
+(defn- test-stylesheet-path
+  [name]
+  (path/resolve (str "src/css_gardener/postcss_transformer/"
+                     "test_stylesheets/"
+                     name)))
+
+(defn- test-stylesheet
+  [name]
+  (let [abs-path (test-stylesheet-path name)]
+    #js {:absolutePath abs-path
+         :content (fs/readFileSync abs-path "utf8")}))
+
+(deftest t-enter-no-absolute-path
+  (testing "yields an error when no 'absolutePath' key exists on the file"
+    (async done
+      (enter #js {:content "hello world"}
+             #js {}
+             (fn [err result]
+               (is (nil? result))
+               (is (= "'absolutePath' key is missing on the input file"
+                      (.-message err)))
+               (done))))))
+
+(deftest t-enter-no-content
+  (testing "yields an error when no 'content' key exists on the file"
+    (async done
+      (enter #js {:absolutePath "/hello/world"}
+             #js {}
+             (fn [err result]
+               (is (nil? result))
+               (is (= "'content' key is missing on the input file"
+                      (.-message err)))
+               (done))))))
+
+(deftest t-enter-applies-postcss-plugins-to-input-file
+  (testing "applies plugins to input file"
+    (async done
+      (enter (test-stylesheet "flex.css")
+             (to-js {:plugins [{:node-module "cssnano"}]})
+             (fn [err result]
+               (is (nil? err))
+               (is (= "/*! comment */.container{flex:1}"
+                      (gobj/get result "content")))
+               (done))))))
+
+(deftest t-enter-applies-options-passed-to-plugins
+  (testing "applies options passed to plugins"
+    (async done
+      (enter (test-stylesheet "flex.css")
+             (to-js {:plugins [{:node-module "cssnano"
+                                :options {:preset ["default" {:discard-comments {:remove-all true}}]}}]})
+             (fn [err result]
+               (is (nil? err))
+               (is (= ".container{flex:1}"
+                      (gobj/get result "content")))
+               (done))))))
+
+(deftest t-exit-yields-file-unmodified
+  (testing "yields the input file unmodified"
+    (async done
+      (exit #js {:absolutePath "/hello/world"
+                 :content "hello world"}
+            #js {}
+            (fn [err result]
+              (is (nil? err))
+              (is (gobj/equals #js {:absolutePath "/hello/world"
+                                    :content "hello world"}
+                               result))
+              (done))))))
