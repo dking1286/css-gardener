b21b169965f0e7a39540a656bd743b4ad5bffb73
Daniel King <daniel.oliver.king@gmail.com>
2020-09-12 19:58:23 -0700

    build(scripts): Migrate for_each_script to Clojure, do the subpackage actions in parallel

--- scripts/css_gardener/scripts/for_each_package.clj
+++ scripts/css_gardener/scripts/for_each_package.clj
@@ -1,3 +1,29 @@
-(ns css-gardener.scripts.for-each-package)
+(ns css-gardener.scripts.for-each-package
+  (:require [clojure.java.io :as io]
+            [clojure.java.shell :refer [sh]]
+            [clojure.string :as string]
+            [css-gardener.scripts.utils :refer [get-packages]]))
 
-;; TODO: Implement me
+(defn -main
+  [command]
+  (->> (get-packages)
+       (pmap (fn [package]
+               (println (str "Running command \""
+                             command
+                             "\" in package "
+                             package))
+               (let [segments (concat (string/split command #" ")
+                                      [:dir (io/file package)])
+                     {:keys [exit out err]} (apply sh segments)]
+                 (if-not (zero? exit)
+                   (do
+                     (println out)
+                     (println err)
+                     (throw (ex-info (str "Failed to run command \""
+                                          command
+                                          "\" in package"
+                                          package)
+                                     {})))
+                   (println out)))))
+       doall)
+  (shutdown-agents))
